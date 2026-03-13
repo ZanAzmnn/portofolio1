1777cb57bc8d24ae87541b4fa4782f285dc9fa59
Fauzan Aziman <pfauzanaziman@gmail.com>
2026-03-13 13:29:41 +0700

    update warna

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       {/* Hero Section - Tetap sama */}
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div className="animate__animated animate__fadeInUp animate__delay-3s">
-          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl hover:bg-cyan-600 transition duration-500 ease-in-out hover:translate-x-4">
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl hover:bg-red-700 transition duration-500 ease-in-out hover:translate-x-4">
             <h1 className="text-2xl font-bold">Selamat Datang 🚀</h1>
           </div>
           <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Fauzan Aziman</h1>
